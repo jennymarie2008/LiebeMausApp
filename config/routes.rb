498d14bbd9deb6d0c4a687525a26c01c2981cde5
jennymarie2008 <jcmauermann@yahoo.de>
2018-09-04 02:22:34 +0200

    4.3 order controller actions & views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'simple_pages/landing_page'
   resources :products
   get 'simple_pages/index'
   get 'simple_pages/about'
   get 'simple_pages/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'simple_pages#index'
+  root 'simple_pages#landing_page'
+  resources :orders, only: [:index, :show, :create, :destroy]
 end
